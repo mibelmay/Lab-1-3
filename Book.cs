c026abdbc6c9d8788db20fad8134b46a31e302c4
Stacy <zemcovannastasia11@gmail.com>
2023-01-21 21:30:40 +0500

    класс Book

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab
+{
+    internal class Book
+    {
+        public uint Id { get; }
+        public string Name { get; }
+        public string AuthorName { get; }
+        public uint PublicationYear { get; }
+        public uint Bookcase { get;  }
+        public uint Bookshelf { get; }
+
+        public Book(uint id, string name, string authorName, uint publicationYear, uint bookcase, uint bookshelf)
+        {
+            Id = id;
+            Name = name;
+            AuthorName = authorName;
+            PublicationYear = publicationYear;
+            Bookcase = bookcase;
+            Bookshelf = bookshelf;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Lab
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Book book1 = new Book(1, "Dracula", "Bram Stoker", 1897, 13, 13);
+            Console.WriteLine(book1.Name);
+        }
+    }
+}
