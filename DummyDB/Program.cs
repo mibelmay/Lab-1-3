314479408b976e7d34ea5f32cb11075dca73b04c
Stacy <zemcovannastasia11@gmail.com>
2023-01-23 03:38:22 +0500

    added class EntryLoader

--- DummyDB/Program.cs
+++ DummyDB/Program.cs
@@ -11,7 +11,11 @@ namespace Lab
 
             string readersFileName = "readers.csv";
             List<Reader> readers = ReadersTableLoader.LoadReaders($".\\{readersFileName}", readersFileName);
-            Console.WriteLine($"{readers[1].FullName}");
+
+            string entriesFileName = "entries.csv";
+            List<Entry> entries = EntryLoader.LoadEntries($".\\{entriesFileName}", entriesFileName, readers, books);
+
+            Console.Write($"{entries[0].Reader.FullName} читает {entries[0].Book.Name} с {entries[0].BorrowDate}");
         }
     }
 }
