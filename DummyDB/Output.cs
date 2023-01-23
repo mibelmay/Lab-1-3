b3376bf1c36558509ce81969b1680a76c3f6a1fb
Stacy <zemcovannastasia11@gmail.com>
2023-01-23 22:33:39 +0500

    finale

--- DummyDB/Output.cs
+++ DummyDB/Output.cs
@@ -4,11 +4,11 @@
     {
         public static void DisplayBooks(List<Book> books, List<Reader> readers, List<Entry> entries)
         {
-            Console.WriteLine("Список всех книг:");
+            Console.WriteLine("Список книг в библиотеке:");
             Console.WriteLine();
             foreach (Book book in books)
             {
-                Console.Write($"\"{book.Name}\"");
+                Console.Write($"\"{book.Name}\", {book.AuthorName}");
                 foreach(Entry entry in entries)
                 {
                     if (book == entry.Book && entry.ReturnDate == null)
