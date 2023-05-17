dfe46af22a876336d567cf9a3de4618335c0afa3
Stacy <zemcovannastasia11@gmail.com>
2023-05-18 00:26:23 +0500

    3 Laba finally done

--- DummyDB/Output.cs
+++ DummyDB/Output.cs
@@ -1,4 +1,6 @@
-﻿namespace Lab
+﻿using System.Reflection.Metadata;
+
+namespace Lab
 {
     internal class Output
     {
@@ -19,6 +21,37 @@
                 }
                 Console.WriteLine();
             }
+        }
+
+        public static void DisplayDB(List<Book> books, List<Reader> readers, List<Entry> entries)
+        {
+            int maxBookAuthor = books.Max(book => book.AuthorName.Length);
+            int maxBookName = books.Max(book => book.Name.Length);
+            int maxReader = readers.Max(reader => reader.FullName.Length);
+            string format = $"| {{0, {maxBookAuthor}}} | {{1, {maxBookName}}} | {{2, {maxReader}}} | {{3, 18}} |";
+
+            //18 - длина DateTime, 13 - кол-во пробелов + кол-во |
+            int tableWidth = maxReader + maxBookName + maxBookAuthor + 18 + 13;
+            Console.WriteLine(new string('-', tableWidth));
+            Console.WriteLine(String.Format(format, "Автор", "Название", "Читает", "Взял"));
+            Console.WriteLine(new string('-', tableWidth));
+            string author;
+            string bookName;
+            string readerName;
+            string date;
+            for (int i = 0; i < entries.Count; i++)
+            {
+                author = entries[i].Book.AuthorName;
+                bookName = entries[i].Book.Name;
+                readerName = entries[i].Reader.FullName;
+                if (entries[i].ReturnDate!=null)
+                {
+                    continue;
+                }
+                date = entries[i].BorrowDate.ToString();
+                Console.WriteLine(String.Format(format, author, bookName, readerName, date));
+            }
+            Console.WriteLine(new string('-', tableWidth));
         }
     }
 }
